a02b7a0f1730780de509c8977d49ac3a8e7a9043
HosKirakosyan <118116663+HosKirakosyan@users.noreply.github.com>
2023-01-26 16:08:00 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-window.onload = function () {
 
-    const menu_btn = document.querySelector('.header_burger')
-    const header_menu = document.querySelector('.menu')
+    const menu_btn = document.getElementById('header_burger')
+    const header_menu = document.getElementById('menu')
+    const burger_background = document.getElementById('burger_background')
+    
 
-    menu_btn.addEventListener('click',function () {
+    document.onclick = function(e){
+        if(e.target.id !== 'header_burger' && e.target.id !== 'menu'){
+            menu_btn.classList.remove('is-active');
+            header_menu.classList.remove('is-active');
+            burger_background.classList.remove('is-active');
+        };
+    };
+
+    menu_btn.onclick = function(){
         menu_btn.classList.toggle('is-active');
         header_menu.classList.toggle('is-active');
-    })
-}
+        burger_background.classList.toggle('is-active');
+        
+    };
